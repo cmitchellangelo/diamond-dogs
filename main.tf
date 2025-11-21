ea72ba88adbdf0ff205ee9e972447a6071334911
Chase <test@test.com>
2025-11-21 13:18:50 -0600

    changing ec2 az from a to b

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "diamond_dogs" {
 resource "aws_subnet" "diamond_dogs" {
   vpc_id     = aws_vpc.diamond_dogs.id
   cidr_block = var.subnet_prefix
-  availability_zone = "us-east-1a"  # Add this: Supported for t3.micro
+  availability_zone = "us-east-1b"  # Add this: Supported for t3.micro
 
   tags = {
     name = "${var.prefix}-subnet"
