4e0201b95d53836dd69a65e7370d9dd439f9fa64
Chase <test@test.com>
2025-11-24 10:36:28 -0600

    adding s3 bucket for images

--- main.tf
+++ main.tf
@@ -323,6 +323,37 @@ resource "aws_route53_record" "www" {
   }
 }
 
+# S3 Bucket for Resume Images
+resource "aws_s3_bucket" "resume_images" {
+  bucket = "$$ {var.project}-resume-images- $${var.environment}"
+}
+
+resource "aws_s3_bucket_public_access_block" "resume_images" {
+  bucket = aws_s3_bucket.resume_images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "resume_images" {
+  bucket = aws_s3_bucket.resume_images.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.resume_images.arn}/*"
+      }
+    ]
+  })
+}
+
 # Outputs
 output "alb_dns_name" {
   description = "ALB DNS name for testing"
