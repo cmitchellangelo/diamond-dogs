4843506e4232acc7525412c36fd705029b5f8106
Chase <test@test.com>
2025-11-24 11:47:23 -0600

    upgrading default action on alb

--- main.tf
+++ main.tf
@@ -269,13 +269,8 @@ resource "aws_lb_listener" "http_redirect" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.diamond_dogs.arn  # Add this
   }
 }
 
